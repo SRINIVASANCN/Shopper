93800788921f246a47da8635eb97b0364814ae21
SRINIVASANCN <157290991+SRINIVASANCN@users.noreply.github.com>
2024-09-16 18:55:47 +0530

    first commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+       <div className="description-navigator">
+          <div className="descriptionbox-nav-box">Description </div>
+          <div className="descriptionbox-nav-box fade">Reviews (122) </div>
+       </div>
+       <div className="descriptionbox-description">
+           <p>U.S. Polo Assn. brand products are authentic and officially sanctioned by the United States Polo Association, the governing body for the sport of polo in the United States since 1890.The U.S. Polo Assn. brand carries clothing for men, women and children.</p>
+           <p>U.S. Polo Assn. brand products are authentic and officially sanctioned by the United States Polo Association, the governing body for the sport of polo in the United States since 1890.The U.S. Polo Assn. brand carries clothing for men, women and children.</p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
